42a24090bb742adfc2059b702058b88b27e892a7
Alex Solano <alexsolano@Alexs-MacBook-Air-6.local>
2025-11-14 20:57:53 -0500

    added EdgeTests.java

---
+++ EdgeTests.java
@@ -0,0 +1,52 @@
+/**
+ * EdgeTests.java
+ * Alex Solano
+ * CS 231 A
+ * 5/5/23
+ * 
+ * Provides tests for Edge.java
+ * 
+ * To compile: javac EdgeTests.java
+ * To run: java -ea EdgeTests
+ */
+
+public class EdgeTests{
+
+    public static void main(String[] args){
+        
+        
+        //test case 1: constuctor, distance(), other(), vertices(), equals()
+        {
+            // given
+            Vertex v1 = new Vertex();   
+            Vertex v2 = new Vertex();   
+            Edge e12 = new Edge(v1, v2, 1);
+
+            // when 
+            System.out.println("After creating an edge between v1 and v2 with a distance of 1.0:");
+            System.out.println("Does an edge exist between v1 and v2? " + (e12 != null));
+            System.out.println("Distance of Edge: " + e12.distance() + " == 1.0");
+            System.out.println("Is the first endpoint of the edge v1? " + (e12.other(v2) == v1));
+            System.out.println("Is the second endpoint of the edge v2? " + (e12.other(v1) == v2));
+            System.out.println("Number of Vertices the edge connects: " + e12.vertices().length + " == 2");
+            System.out.println("Is the first endpoint of the edge v1? " + (e12.vertices()[0] == v1));
+            System.out.println("Is the second endpoint of the edge v2? " + (e12.vertices()[1] == v2));
+
+            Edge e21 = new Edge(v2, v1, 1);
+            
+            System.out.println("After creating a new Edge between v1 and v2 with the same magniture, but with the order reverse:");
+            System.out.println("Both edges should be equal: " + (e12.equals(e21) == true) + " == true");
+            
+            // then
+            assert e12 != null : "Error in Edge::Edge()";
+            assert e12.distance() == 1 : "Error in Edge::Edge()";
+            assert e12.other(v2) == v1 : "Error in Edge::other()";
+            assert e12.other(v1) == v2 : "Error in Edge::other()";
+            assert e12.vertices().length == 2 : "Error in Edge::vertices()";
+            assert e12.vertices()[0] == v1 : "Error in Edge::vertices()";
+            assert e12.vertices()[1] == v2 : "Error in Edge::vertices()";
+            assert e12.equals(e21) == true : "Error in Edge::equals()";
+        }
+        System.out.println("*** Done testing Edge.java! ***");
+    }
+}
