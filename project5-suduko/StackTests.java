f068023f4ef5dd237b85662435643d56b27fd205
Lucas <lplefe26@colby.edu>
2025-11-16 10:03:40 -0500

    Expanded testing of pop and peek

--- project5-suduko/StackTests.java
+++ project5-suduko/StackTests.java
@@ -39,12 +39,23 @@ public class StackTests {
             stack.push(3);
 
             // when
-            int value = stack.pop();
-            System.out.println("Popped value: " + value);
+            int value3 = stack.pop();
+            int size1 = stack.size();
+            int value2 = stack.pop();
+            int value = stack.pop();    
+            System.out.println("First popped value: " + value3);
+            System.out.println("Second popped value: " + value2);
+            System.out.println("Third popped value: " + value);
+            System.out.println("Size after first pop: " + size1);
+            System.out.println("Size after all pops: " + stack.size());
+
 
             // then
-            assert value == 3 : "Error in Stack::pop()";
-            assert stack.size() == 2 : "Error in Stack::pop() size check";
+            assert value3 == 3 : "Error in Stack::pop()";
+            assert value2 == 2 : "Error in Stack::pop()";
+            assert value == 1 : "Error in Stack::pop()";
+            assert size1 == 2 : "Error in Stack::pop() size check";
+            assert stack.size() == 0 : "Error in Stack::pop() size check";
         }
 
         // case 4: testing peek()
@@ -57,10 +68,13 @@ public class StackTests {
 
             // when
             int value = stack.peek();
+            int value2 = stack.peek();
             System.out.println("Peeked value: " + value);
+            System.out.println("Peeked value again: " + value2);
 
             // then
             assert value == 3 : "Error in Stack::peek()";
+            assert value2 == 3 : "Error in Stack::peek()";
             assert stack.size() == 3 : "Error in Stack::peek() size check";
         }
     }
